fbcb5be0db35410ac14aea4d0893fc4ea9e72d60
Arina Kunchenko <kunchari@fit.cvut.cz>
2026-07-10 16:59:36 +0200

    feat: All integers explained

--- week-01-vars-io/integers.c
+++ week-01-vars-io/integers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <limits.h>
-#include <math.h>
 
  /*
  * Variables may have different names but are supposed to start from either:
@@ -8,17 +7,16 @@
  * 2. underscore
  * and followed by letters, underscores and digits.
  * Specific names known to the compiler won't go as well (ex.int).
- * Different writing - different name (ex. Sum, sum, SUM)
+ * Different name - different variable (ex. Sum, sum, SUM)
  */
  
  // Let's go through int, short, long, long long, unsigned and char
  
-#define EPSILON 1e-5
- 
 int main(int argc, char * argv[]) {
 	
 	/*INT*/
-	printf("Integer [int]: \n");
+	printf("Integer [int]: \n\n");
+	 
 	 
 	// declared but not uninitialized int, consists random trash from memory
 	
@@ -29,8 +27,7 @@ int main(int argc, char * argv[]) {
 	
 	// Now it's initialized!
 	number = 37;
-		
-	int number_1 = 0;
+	
 	int number_2 = -1000000000;
 	
 	// Twice declarized!
@@ -42,13 +39,13 @@ int main(int argc, char * argv[]) {
 	// ^ integer overflow error.
 	// Could be written as "number_4 += 1;"
 
-	printf("Number 1: %d\nNumber 2: %d\n", number_1, number_2);
-	printf("Number 3: %d <- the biggest possible storred int\n", number_3);
+	printf("Number 1: %d\nNumber 2: %d\n", number, number_2);
+	printf("The biggest possible storred int: %d\n", number_3);
 	printf("Integer overflow: %d\n", number_4);
 	
 	
 	/*SHORT*/
-	printf("\nShort integer [short]: \n");
+	printf("\nShort integer [short]: \n\n");
 
 	
 	short small_num = 42;
@@ -64,13 +61,95 @@ int main(int argc, char * argv[]) {
 	
 	printf("Short int: %hd\n", small_num);
 	printf("Short overflow: (expected 32767): %hd\n", small_overflow);
-	printf("Big sum of 2 possitive: %hd\n", invalid_sum);
+	printf("Big sum of 2 possitive: (expected 32780) %hd\n", invalid_sum);
 	// ^ while %d or %i is absolutely working, there is a %hd - half decimal (2B)
 	
-	/*LONG*/
 	
-	long bigger_num = 100000000000;
+	/*LONG*/
+	printf("\nLong integer [long]: \n\n");
+
+	
+	long bigger_num = 100000000000000;
+	// ^ long is a type "in between" int and long long. 
+	// Depending on the system, it may store numbers in 4B or 8B.
+	// On Linux its size is 8B. That means 2^64 different numbers.
+	
+	long max_long = LONG_MAX;
+	// Overflow also works here the same way.
+	
+	printf("Long int: %ld\n", bigger_num);
+	printf("Biggest long int: %ld (May differ on different systems)\n", max_long);
+	// ^ uses %ld as long d
+	
+	/*LONG LONG*/
+	printf("\nLong long integer [long long]: \n\n");
+	
+	long long even_bigger = 1000000000000000000;
+	// ^ this is the biggest possible integer type that can be stored in 8B - 2^64
+	
+	long long the_biggest_int = LLONG_MAX;
+	
+	printf("Long long int: %lld\n", even_bigger);
+	printf("The biggest possible signed stored integer: %lld\n (On Linux is the same with Long)\n", the_biggest_int);
+	// ^ uses %lld as long long d
+	
+	
+	/*UNSIGNED*/
+	printf("\nUnsigned integers [unsigned int/short/long/long long]: \n\n");
+	
+	
+	// Basically unsigned number means that the first (leftmost) bit is used to store the number.
+	// Signed numbers are cut in 1 bit to know if it's negative or possitive. 
+	// That means, that the stored numbers are bigger (ex. int -> 0 - 2^32 -1) but only possitive.
+	// The capacity is the same for all integers 
+	
+	unsigned int un_num = UINT_MAX;
+	// ^ can be also declared as "unsigned int"
+	
+	unsigned short 		un_short 		=	USHRT_MAX;
+	unsigned long 		un_long 		=	ULONG_MAX;
+	unsigned long long 	un_long_long 	=	ULLONG_MAX;
+	
+	printf("This is the biggest int: %u\n", un_num);
+	printf("This is the biggest short int: %hu\n", un_short);
+	printf("This is the biggest long int: %lu\n", un_long);
+	printf("This is the biggest long long int: %llu\n", un_long_long);
+	
+	printf("The smallest number is always 0!\n");
+
+	unsigned over_num = UINT_MAX + 2;
+	// ^ this won't overflow to the negative number! 
+	// The overflow flag will be set and it will be back counting from 0
+	
+	printf("This is overflow (max + 2): %u\n", over_num);
+	
+	
+	/*CHAR*/
+	printf("\nChar: \n\n");
+	
+	
+	char a = 'a';
+	// ^ char is a small integer. Takes only a byte - 8 bits.
+	// Usually represents a character but still is a number.
+	// By defaults char is neither signed nor unsigned. Different systems interpret it differently.
+	// This may lead to an error when checking = -1 as on ARM it never will happen.
+	// Linux considers it signed. This means -128 - 127 range.
+	// Even this 'a' is a number that is ASCII encoded. ('a' = 61 (hex) or 97 (dec))
+	
+	printf("Here is a character: %c\n", a);
+	printf("Here is a character printed as a number: %d\n", a);
+	
+	// What's interesting, as a common number, character allows operations.
+	// For example, add. Or, a funny thing, I can get a real number by substracting '0'
+	
+	char char_num = '6';
+	char_num -= '0';
+	// ^ so it works like '6' = 36 and '0' = 30 -> 36-30 = 6.
+	// Or, well, the distance between those characters is the same as the between the numbers
+	
+	printf("Here is '6' - '0': %d\n", char_num);
+	printf("Here is whatever it is in ASCII multipleid by 10: %d\n", char_num * 10);
+	
 	
 	return 0;
 }
-
