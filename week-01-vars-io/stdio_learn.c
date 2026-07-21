dab685405f9d62c22e9db6c903db988aa6379b68
Arina Kunchenko <kunchari@fit.cvut.cz>
2026-07-21 11:57:28 +0200

    feat: file function

--- week-01-vars-io/stdio_learn.c
+++ week-01-vars-io/stdio_learn.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void file();
+
 int main(int argc, int * argv[]) {
-    
+
     return 0;
+}
+
+void file() {
+    FILE * file = fopen("example.txt", "r");
+    // ^ FILE is a complex data structure used as pointer to files.
+    // Copying with  FILE f = * file is NOT allowed!
+    // It keeps (including POSIX file descriptor):
+    // 1. Character width
+    // 2.
 }
